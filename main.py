1b9e224b4541e28415a42ae6eb0b6a8751eb6239
Stewart Perrygrove <sperrygrove@bink.com>
2021-03-10 20:19:53 +0000

    Use authlib instead of own-rolled sso module

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 from datetime import datetime
 
-from flask import Flask, session
+from flask import Flask, session, url_for, redirect
 from flask_admin import Admin, expose, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 import settings
-from azure_sso import AzureSSO
+from authlib.integrations.flask_client import OAuth
 from db import load_session, User, MembershipCard, Voucher
 
 app = Flask(__name__)
 db_session = load_session()
 app.config.from_object(settings)
-sso = AzureSSO(app)
 
+oauth = OAuth(app)
+oauth.register(
+    'bpl',
+    server_metadata_url=settings.OAUTH_SERVER_METADATA_URL,
+    client_kwargs={'scope': 'openid profile email'}
+)
 # set optional bootswatch theme
 app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
 
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorize', _external=True)
+    return oauth.bpl.authorize_redirect(redirect_uri)
+
+
+@app.route('/logout')
+def logout():
+    # session['user'] will always be set again as long
+    # as your AAD session is still alive.
+    session.pop('user', None)
+    return redirect('/admin')
+
+
+@app.route('/admin/callback')
+def authorize():
+    token = oauth.bpl.authorize_access_token()
+    userinfo = oauth.bpl.parse_id_token(token)
+    session["user"] = userinfo
+    return redirect('/admin')
+
 
 class AuthorisedModelView(ModelView):
-
     def is_accessible(self):
         try:
             is_not_expired = session["user"]["exp"] >= datetime.utcnow().timestamp()
@@ -28,12 +53,15 @@ class AuthorisedModelView(ModelView):
         except KeyError:
             return False
 
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+
 
 class MyAdminIndexView(AdminIndexView):
-
     @expose('/')
-    @sso.login_required()
     def index(self):
+        if 'user' not in session:
+            return redirect('/login')
         return super(MyAdminIndexView, self).index()
 
 
--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 POSTGRES_DSN = "postgresql://postgres@localhost:5555/test_fastapi"
+# POSTGRES_DSN = "postgresql://postgres:postgres@localhost:5432/test_fastapi"
 
-CLIENT_SECRET = "EMq_4c.h8Pim7.ibmb2-.xxB1n6l-o2wj_"
-CLIENT_ID = "2043ed95-8a99-45c9-93b5-71317a50a3ee"
-AUTHORITY = "https://login.microsoftonline.com/a6e2367a-92ea-4e5a-b565-723830bcc095"
+BPL_CLIENT_SECRET = "EMq_4c.h8Pim7.ibmb2-.xxB1n6l-o2wj_"
+BPL_CLIENT_ID = "2043ed95-8a99-45c9-93b5-71317a50a3ee"
+OAUTH_SERVER_METADATA_URL = 'https://login.microsoftonline.com/a6e2367a-92ea-4e5a-b565-723830bcc095/v2.0/.well-known/openid-configuration'
 
-REDIRECT_PATH = "/admin/callback"  # It will be used to form an absolute URL
 SECRET_KEY = "476dd64f-0a46-47e4-b094-8e2743ebe678"
 SESSION_TYPE = "filesystem"
