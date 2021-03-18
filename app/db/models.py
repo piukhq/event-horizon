3cb016b54c5c1d98ce38e166f6b7620b9390c073
Francesco Milani <fmilani@bink.com>
2021-03-18 11:53:02 +0200

    add temp card number length validation

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import validates
 
 from app.db.session import engine
 
@@ -11,6 +12,11 @@ Base = automap_base()
 class Merchant(Base):  # type: ignore
     __tablename__ = "merchant"
     created_at = Column(DateTime, default=datetime.now)
+
+    @validates('card_number_length')
+    def validate_card_number_length(self, key: str, value: int) -> int:
+        assert 26 >= value >= 12
+        return value
 
     def __str__(self) -> str:
         return self.name
