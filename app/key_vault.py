9f5f0c19a3db83986f3257cc52c6dde74405f357
Stewart Perrygrove <sperrygrove@bink.com>
2021-03-31 17:57:32 +0100

    allow SECRET_KEY to live in the vault

--- app/key_vault.py
+++ app/key_vault.py
@@ -1,45 +1,48 @@
 import json
 
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
 
 class KeyVault:
-    def __init__(self, *, vault_url: Optional[str] = None, client: Optional[SecretClient] = None) -> None:
+    def __init__(self, vault_url: Optional[str] = None, /, *, client: Optional[SecretClient] = None) -> None:
         if not (client or vault_url):
             raise ValueError("must provide either vault_url or client")
         self.client = client or SecretClient(vault_url=vault_url, credential=DefaultAzureCredential())
 
-    def get_secret(self, name: str, key: Optional[str] = "value") -> str:
-        return self.get_secrets({name: key})[name]
-
-    def get_secrets(self, name_key_map: Dict[str, Optional[str]]) -> Dict[str, Any]:
-        """Return the content of secrets in the vault as a dict keyed by
-        requested secret name.
+    def get_secret(self, name: str, /, *, key: Optional[str] = "value") -> Any:
+        """Return the content of a secret in the vault.
 
         Secrets may be a mapping or other JSON serializable objects. Obtain
-        specific elements of a secret mapping by providing a key.
+        a specific element of a secret mapping by providing a key.
 
-        Example secrets named 'secret-password', 'secret-cert',
-        'secret-string-1' and 'string-secret-2' in the vault respectively:
+        Example secrets named 'secret1', 'secret2', 'secret3', 'secret4' and
+        'secret5' in the vault respectively:
 
-            * '{"value": "!#123456789!#"}'
-            * '{"key": "--- RSA PRIVATE KEY ---\\nkey goes here....."}'
-            * '"dcc34040b3d145eb92e899eae2b0d886"'
+            * '{"value": "123456789"}'
+            * '{"key": "--- RSA PRIVATE KEY ---\\nkey matter..."}'
+            * '"dcc34040b3d145eb92e899eae2b0d886"'  # json string
             * '{"value": "f0fb00f0741b45929660"}'
+            * "44ef3c2b18b4484dbb9229226beaa98c"  # normal string
 
-        >>> get_secrets({"secret-password": "value", "secret-cert": "key",
-        ... "secret-string1": None, "secret-string2": None})
-        {"secret-password": "!#123456789!#",
-         "secret-cert": "--- RSA PRIVATE KEY ---\\nkey goes here.....",
-         "secret-string-1": "dcc34040b3d145eb92e899eae2b0d886"}
-         "secret-string-2": {"value": "f0fb00f0741b45929660"}}
+        >>> get_secret("secret1", "value")
+        123456789
+        >>> get_secret("secret2", "key")
+        --- RSA PRIVATE KEY ---\\nkey matter...
+        >>> get_secret("secret3", None)
+        dcc34040b3d145eb92e899eae2b0d886
+        >>> get_secret("secret4, None)
+        {"value": "f0fb00f0741b45929660"}
+        >>> get_secret("secret5, None)
+        44ef3c2b18b4484dbb9229226beaa98c
 
         """
-        secrets: Dict[str, str] = {}
-        for name, key in name_key_map.items():
-            val = json.loads(self.client.get_secret(name).value)
-            secrets[name] = val[key] if key is not None else val
-        return secrets
+        secret = self.client.get_secret(name)
+        try:
+            val = json.loads(secret.value)
+        except json.decoder.JSONDecodeError:
+            return secret.value
+        else:
+            return val[key] if key is not None else val
