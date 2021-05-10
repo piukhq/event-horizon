cd35607da2b43988b36ba9bcdc59e62c2da2dc46
Francesco Milani <fmilani@bink.com>
2021-05-10 17:39:32 +0300

    set utc time default for onupdate

--- app/vela/db/models.py
+++ app/vela/db/models.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-from sqlalchemy import Column, DateTime, func
+from sqlalchemy import Column, DateTime, text
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.sql.schema import MetaData
 
@@ -20,7 +18,12 @@ class RetailerRewards(Base):  # type: ignore
 class Campaign(Base):  # type: ignore
     __tablename__ = "campaign"
 
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=datetime.utcnow(), nullable=False)
+    updated_at = Column(
+        DateTime,
+        server_default=text("TIMEZONE('utc', CURRENT_TIMESTAMP)"),
+        onupdate=text("TIMEZONE('utc', CURRENT_TIMESTAMP)"),
+        nullable=False,
+    )
 
     def __str__(self) -> str:
         return self.name
