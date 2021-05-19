5cb6062ae0f3c2cbab43f1805a134f01645eff01
Stewart Perrygrove <sperrygrove@bink.com>
2021-05-19 17:38:36 +0100

    BPL-69 place created_at and updated_at at end of table

--- app/admin/model_views.py
+++ app/admin/model_views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 from flask import abort, redirect, session, url_for
 from flask_admin.contrib.sqla import ModelView
@@ -57,3 +57,13 @@ class BaseModelView(AuthorisedModelView):
     form_widget_args = {"created_at": {"disabled": True}, "updated_at": {"disabled": True}}
     column_default_sort = ("created_at", True)
     form_excluded_columns = ("created_at", "updated_at")
+
+    def get_list_columns(self) -> List[str]:
+        # Shunt created_at and updated_at to the end of the table
+        list_columns = super().get_list_columns()
+        for name in ("created_at", "updated_at"):
+            for i, (col_name, _disp_name) in enumerate(list_columns):
+                if col_name == name:
+                    list_columns += [list_columns.pop(i)]
+                    break
+        return list_columns
