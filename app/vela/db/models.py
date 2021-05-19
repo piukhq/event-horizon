c5fad599f2b5c2323a794f931d1040013a379ab2
Stewart Perrygrove <sperrygrove@bink.com>
2021-05-19 17:45:47 +0100

    BPL-69 allow mixin to be shared

--- app/vela/db/models.py
+++ app/vela/db/models.py
@@ -1,23 +1,11 @@
-from sqlalchemy import Column, DateTime, text
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import MetaData
 
+from app.db import UpdatedAtMixin
+
 metadata = MetaData()
 Base = automap_base(metadata=metadata)
-
-
-class UpdatedAtMixin:
-    """
-    Required for any model that has an updated_at field.
-    """
-
-    updated_at = Column(
-        DateTime,
-        server_default=text("TIMEZONE('utc', CURRENT_TIMESTAMP)"),
-        onupdate=text("TIMEZONE('utc', CURRENT_TIMESTAMP)"),
-        nullable=False,
-    )
 
 
 class RetailerRewards(Base):  # type: ignore
