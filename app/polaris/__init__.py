b647867f6694df4de8c7ebadaea4fceb6a80cd18
Stewart Perrygrove <sperrygrove@bink.com>
2021-05-21 15:12:11 +0100

    Group tables by project in nav menu

--- app/polaris/__init__.py
+++ app/polaris/__init__.py
@@ -8,15 +8,28 @@ if TYPE_CHECKING:
 
 
 def register_polaris_admin(event_horizon_admin: "Admin") -> None:
+    polaris_menu_title = "Customer Management"
     event_horizon_admin.add_view(
-        AccountHolderAdmin(AccountHolder, db_session, "Account Holders", endpoint="account-holders")
+        AccountHolderAdmin(
+            AccountHolder, db_session, "Account Holders", endpoint="account-holders", category=polaris_menu_title
+        )
     )
     event_horizon_admin.add_view(
-        AccountHolderProfileAdmin(AccountHolderProfile, db_session, "Profiles", endpoint="profiles")
+        AccountHolderProfileAdmin(
+            AccountHolderProfile, db_session, "Profiles", endpoint="profiles", category=polaris_menu_title
+        )
     )
     event_horizon_admin.add_view(
-        RetailerConfigAdmin(RetailerConfig, db_session, "Retailers' Config", endpoint="retailers-config")
+        RetailerConfigAdmin(
+            RetailerConfig, db_session, "Retailers' Config", endpoint="retailers-config", category=polaris_menu_title
+        )
     )
     event_horizon_admin.add_view(
-        EnrolmentCallbackAdmin(EnrolmentCallback, db_session, "Enrolment Callbacks", endpoint="enrolment-callbacks")
+        EnrolmentCallbackAdmin(
+            EnrolmentCallback,
+            db_session,
+            "Enrolment Callbacks",
+            endpoint="enrolment-callbacks",
+            category=polaris_menu_title,
+        )
     )
