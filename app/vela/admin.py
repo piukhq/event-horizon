53053e30b83794af7552c11326e896a8fe134f07
Francesco Milani <fmilani@bink.com>
2021-07-05 15:09:40 +0300

    BPL-159 fix style

--- app/vela/admin.py
+++ app/vela/admin.py
@@ -3,7 +3,7 @@ import wtforms
 from flask_admin.model import typefmt  # type: ignore
 from wtforms.validators import DataRequired
 
-from app.admin.model_views import AuthorisedModelView, BaseModelView
+from app.admin.model_views import BaseModelView
 from app.vela.validators import validate_campaign_earn_inc_is_tx_value, validate_earn_rule_increment
 
 
