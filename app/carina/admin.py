23b3c4f185b0da967cab3ff7326262c2194de4f1
David Felce <dfelce@bink.com>
2021-07-14 13:09:26 +0100

    BPL-110: voucher table admin

--- app/carina/admin.py
+++ app/carina/admin.py
@@ -1,3 +1,4 @@
+from flask import Markup
 from wtforms.validators import NumberRange
 
 from app.admin.model_views import BaseModelView
@@ -13,3 +14,13 @@ class VoucherConfigAdmin(BaseModelView):
             ],
         }
     }
+
+
+class VoucherAdmin(BaseModelView):
+    column_searchable_list = ("voucherconfig.id", "voucherconfig.voucher_type_slug", "voucherconfig.retailer_slug")
+    column_labels = dict(voucherconfig="Voucher config")
+    column_filters = ("voucherconfig.retailer_slug", "voucherconfig.voucher_type_slug", "allocated")
+    column_formatters = dict(
+        voucherconfig=lambda v, c, model, p: Markup.escape(model.voucherconfig)
+        + Markup("<br />" + f"({model.voucherconfig.id})")
+    )
--- app/carina/db/models.py
+++ app/carina/db/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import MetaData
 
 from app.db import UpdatedAtMixin
@@ -9,3 +10,12 @@ Base = automap_base(metadata=metadata)
 
 class VoucherConfig(Base, UpdatedAtMixin):  # type: ignore
     __tablename__ = "voucher_config"
+
+    voucher_collection = relationship("Voucher", backref="vouchers", cascade="all, delete-orphan")
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}({self.retailer_slug}, " f"{self.voucher_type_slug}, {self.validity_days})"
+
+
+class Voucher(Base, UpdatedAtMixin):  # type: ignore
+    __tablename__ = "voucher"
