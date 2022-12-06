2c2cc994c78a849420e27965d4b27fa8d33149b7
Francesco Milani <fmilani@bink.com>
2022-12-06 17:38:09 +0200

    BPL-118 delete retailer custom action

---
+++ event_horizon/polaris/custom_actions.py
@@ -0,0 +1,145 @@
+import logging
+
+from dataclasses import dataclass
+
+from flask import flash
+from sqlalchemy import func
+from sqlalchemy.exc import DBAPIError
+from sqlalchemy.future import select
+
+from event_horizon.admin.utils import SessionDataMethodsMixin
+from event_horizon.carina.db.models import Retailer
+from event_horizon.carina.db.session import db_session as carina_db_session
+from event_horizon.hubble.db.models import Activity
+from event_horizon.hubble.db.session import db_session as hubble_db_session
+from event_horizon.polaris.db.models import AccountHolder, AccountHolderReward, RetailerConfig
+from event_horizon.polaris.db.session import db_session as polaris_db_session
+from event_horizon.polaris.forms import DeleteRetailerActionForm
+from event_horizon.vela.db.models import Campaign, RetailerRewards
+from event_horizon.vela.db.session import db_session as vela_db_session
+
+
+@dataclass
+class SessionData(SessionDataMethodsMixin):
+    retailer_name: str
+    retailer_slug: str
+    polaris_retailer_id: int
+
+
+class DeleteRetailerAction:
+    logger = logging.getLogger("delete-retailer-action")
+
+    def __init__(self) -> None:
+        self.form = DeleteRetailerActionForm()
+        self._session_data: SessionData | None = None
+
+    @property
+    def session_data(self) -> SessionData:
+        if not self._session_data:
+            raise ValueError("session_data is not set")
+
+        return self._session_data
+
+    @session_data.setter
+    def session_data(self, value: str) -> None:
+        self._session_data = SessionData.from_base64_str(value)
+
+    def affected_account_holders_count(self) -> int:  # pragma: no cover
+        return polaris_db_session.scalar(
+            select(func.count(AccountHolder.id)).where(
+                AccountHolder.retailer_id == self.session_data.polaris_retailer_id
+            )
+        )
+
+    def affected_rewards_count(self) -> int:  # pragma: no cover
+        return polaris_db_session.scalar(
+            select(func.count(AccountHolderReward.id)).where(
+                AccountHolderReward.account_holder_id == AccountHolder.id,
+                AccountHolder.retailer_id == self.session_data.polaris_retailer_id,
+            )
+        )
+
+    def affected_campaigns_slugs(self) -> list[str]:  # pragma: no cover
+        return vela_db_session.scalars(
+            select(Campaign.slug).where(
+                Campaign.retailer_id == RetailerRewards.id,
+                RetailerRewards.slug == self.session_data.retailer_slug,
+            )
+        ).all()
+
+    @staticmethod
+    def _get_retailer_by_id(retailer_id: int) -> RetailerConfig:  # pragma: no cover
+        return polaris_db_session.get(RetailerConfig, retailer_id)
+
+    def validate_selected_ids(self, ids: list[str]) -> str | None:
+        if not ids:
+            return "no retailer selected."
+
+        if len(ids) > 1:
+            return "Only one Retailer allowed for this action"
+
+        retailer = self._get_retailer_by_id(int(ids[0]))
+
+        if retailer.status == "ACTIVE":
+            return "Only non active Retailers allowed for this action"
+
+        self._session_data = SessionData(
+            retailer_name=retailer.name, retailer_slug=retailer.slug, polaris_retailer_id=retailer.id
+        )
+
+        return None
+
+    def _delete_polaris_retailer_data(self) -> None:  # pragma: no cover
+        polaris_db_session.execute(
+            RetailerConfig.__table__.delete().where(RetailerConfig.slug == self.session_data.retailer_slug)
+        )
+        polaris_db_session.flush()
+
+    def _delete_vela_retailer_data(self) -> None:  # pragma: no cover
+        vela_db_session.execute(
+            RetailerRewards.__table__.delete().where(RetailerRewards.slug == self.session_data.retailer_slug)
+        )
+        vela_db_session.flush()
+
+    def _delete_carina_retailer_data(self) -> None:  # pragma: no cover
+        carina_db_session.execute(Retailer.__table__.delete().where(Retailer.slug == self.session_data.retailer_slug))
+        carina_db_session.flush()
+
+    def _delete_hubble_retailer_data(self) -> None:  # pragma: no cover
+        hubble_db_session.execute(
+            Activity.__table__.delete().where(Activity.retailer == self.session_data.retailer_slug)
+        )
+        hubble_db_session.flush()
+
+    def delete_retailer(self) -> None:
+        if not self.form.acceptance.data:
+            flash("User did not agree to proceed, action halted.")
+            return
+
+        try:
+            self._delete_polaris_retailer_data()
+            self._delete_vela_retailer_data()
+            self._delete_carina_retailer_data()
+            self._delete_hubble_retailer_data()
+        except DBAPIError:
+            polaris_db_session.rollback()
+            vela_db_session.rollback()
+            carina_db_session.rollback()
+            hubble_db_session.rollback()
+
+            self.logger.exception(
+                "Exception while trying to delete retailer %s (%d)",
+                self.session_data.retailer_slug,
+                self.session_data.polaris_retailer_id,
+            )
+            flash("Something went wrong, database changes rolled back", category="error")
+            return
+
+        polaris_db_session.commit()
+        vela_db_session.commit()
+        carina_db_session.commit()
+        hubble_db_session.commit()
+        flash(
+            f"All rows related to retailer {self.session_data.retailer_name} ({self.session_data.polaris_retailer_id}) "
+            "have been deleted."
+        )
---
+++ tests/polaris/test_custom_actions.py
@@ -0,0 +1,222 @@
+import base64
+import pickle
+
+from typing import Generator, NamedTuple
+from unittest.mock import MagicMock
+
+import pytest
+
+from flask import Flask
+from pytest_mock import MockerFixture
+from sqlalchemy.exc import DataError
+
+from event_horizon.polaris.custom_actions import DeleteRetailerAction, SessionData
+
+
+class MockedRetailerConfig(NamedTuple):
+    id: int
+    slug: str
+    name: str
+    status: str
+
+
+class SessionTestData(NamedTuple):
+    value: SessionData
+    b64str: str
+
+
+class DeleteActionMockedDBCalls(NamedTuple):
+    get_retailer_by_id: MagicMock
+    delete_polaris_retailer_data: MagicMock
+    delete_vela_retailer_data: MagicMock
+    delete_carina_retailer_data: MagicMock
+    delete_hubble_retailer_data: MagicMock
+
+
+class DBSessionsMocks(NamedTuple):
+    polaris_db_session: MagicMock
+    vela_db_session: MagicMock
+    carina_db_session: MagicMock
+    hubble_db_session: MagicMock
+
+
+@pytest.fixture
+def test_session_data() -> SessionTestData:
+    session_data = SessionData(
+        retailer_slug="test-retailer",
+        retailer_name="Test Retailer",
+        polaris_retailer_id=1,
+    )
+
+    return SessionTestData(
+        value=session_data,
+        b64str=base64.b64encode(pickle.dumps(session_data)).decode(),
+    )
+
+
+@pytest.fixture
+def delete_action() -> Generator[DeleteRetailerAction, None, None]:
+    app = Flask(__name__)
+    app.secret_key = "random string"
+    with app.app_context(), app.test_request_context():
+        yield DeleteRetailerAction()
+
+
+@pytest.fixture
+def delete_action_mocks(mocker: MockerFixture, delete_action: DeleteRetailerAction) -> DeleteActionMockedDBCalls:
+    return DeleteActionMockedDBCalls(
+        get_retailer_by_id=mocker.patch.object(
+            delete_action,
+            "_get_retailer_by_id",
+            return_value=MockedRetailerConfig(
+                id=1,
+                slug="test-retailer",
+                name="Test Retailer",
+                status="TEST",
+            ),
+        ),
+        delete_polaris_retailer_data=mocker.patch.object(delete_action, "_delete_polaris_retailer_data"),
+        delete_vela_retailer_data=mocker.patch.object(delete_action, "_delete_vela_retailer_data"),
+        delete_carina_retailer_data=mocker.patch.object(delete_action, "_delete_carina_retailer_data"),
+        delete_hubble_retailer_data=mocker.patch.object(delete_action, "_delete_hubble_retailer_data"),
+    )
+
+
+@pytest.fixture
+def db_sessions_mocks(mocker: MockerFixture) -> DBSessionsMocks:
+    return DBSessionsMocks(
+        polaris_db_session=mocker.patch("event_horizon.polaris.custom_actions.polaris_db_session"),
+        vela_db_session=mocker.patch("event_horizon.polaris.custom_actions.vela_db_session"),
+        carina_db_session=mocker.patch("event_horizon.polaris.custom_actions.carina_db_session"),
+        hubble_db_session=mocker.patch("event_horizon.polaris.custom_actions.hubble_db_session"),
+    )
+
+
+def test_session_data_methods(test_session_data: SessionTestData) -> None:
+    assert test_session_data.value.to_base64_str() == test_session_data.b64str
+    assert SessionData.from_base64_str(test_session_data.b64str) == test_session_data.value
+
+
+def test_delete_retailer_action_session_form_data(
+    delete_action: DeleteRetailerAction, test_session_data: SessionTestData
+) -> None:
+
+    with pytest.raises(ValueError) as ex_info:
+        delete_action.session_data  # pylint: disable=pointless-statement
+
+    assert ex_info.value.args[0] == "session_data is not set"
+
+    delete_action.session_data = test_session_data.b64str  # type: ignore [assignment]
+    assert delete_action.session_data == test_session_data.value
+
+
+def test_validate_selected_ids_no_ids(delete_action: DeleteRetailerAction) -> None:
+    assert delete_action.validate_selected_ids([]) == "no retailer selected."
+
+
+def test_validate_selected_ids_too_many_ids(delete_action: DeleteRetailerAction) -> None:
+    assert delete_action.validate_selected_ids(["1", "2"]) == "Only one Retailer allowed for this action"
+
+
+def test_validate_selected_ids_wrong_status(
+    delete_action: DeleteRetailerAction, delete_action_mocks: DeleteActionMockedDBCalls
+) -> None:
+    delete_action_mocks.get_retailer_by_id.return_value = MockedRetailerConfig(
+        id=1, slug="test-retailer", name="Test Retailer", status="ACTIVE"
+    )
+    assert delete_action.validate_selected_ids(["1"]) == "Only non active Retailers allowed for this action"
+    delete_action_mocks.get_retailer_by_id.assert_called_once_with(1)
+
+
+def test_delete_retailer_ok_user_agreed(
+    mocker: MockerFixture,
+    delete_action: DeleteRetailerAction,
+    test_session_data: SessionTestData,
+    db_sessions_mocks: DBSessionsMocks,
+    delete_action_mocks: DeleteActionMockedDBCalls,
+) -> None:
+    assert delete_action.validate_selected_ids(["1"]) is None
+    assert delete_action.session_data == test_session_data.value
+    delete_action_mocks.get_retailer_by_id.assert_called_once_with(1)
+
+    mocked_flash = mocker.patch("event_horizon.polaris.custom_actions.flash")
+    mocked_logger = mocker.patch.object(delete_action, "logger")
+    delete_action.form.acceptance.data = True
+
+    delete_action.delete_retailer()
+
+    delete_action_mocks.delete_polaris_retailer_data.assert_called_once()
+    delete_action_mocks.delete_vela_retailer_data.assert_called_once()
+    delete_action_mocks.delete_carina_retailer_data.assert_called_once()
+    delete_action_mocks.delete_hubble_retailer_data.assert_called_once()
+
+    for field in db_sessions_mocks:
+        field.commit.assert_called_once()
+        field.rollback.assert_not_called()
+
+    mocked_flash.assert_called_once_with(
+        f"All rows related to retailer {test_session_data.value.retailer_name} "
+        f"({test_session_data.value.polaris_retailer_id}) have been deleted."
+    )
+    mocked_logger.exception.assert_not_called()
+
+
+def test_delete_retailer_ok_user_disagreed(
+    mocker: MockerFixture,
+    delete_action: DeleteRetailerAction,
+    test_session_data: SessionTestData,
+    db_sessions_mocks: DBSessionsMocks,
+    delete_action_mocks: DeleteActionMockedDBCalls,
+) -> None:
+    assert delete_action.validate_selected_ids(["1"]) is None
+    assert delete_action.session_data == test_session_data.value
+    delete_action_mocks.get_retailer_by_id.assert_called_once_with(1)
+
+    mocked_flash = mocker.patch("event_horizon.polaris.custom_actions.flash")
+    mocked_logger = mocker.patch.object(delete_action, "logger")
+    delete_action.form.acceptance.data = False
+
+    delete_action.delete_retailer()
+
+    delete_action_mocks.delete_polaris_retailer_data.assert_not_called()
+    delete_action_mocks.delete_vela_retailer_data.assert_not_called()
+    delete_action_mocks.delete_carina_retailer_data.assert_not_called()
+    delete_action_mocks.delete_hubble_retailer_data.assert_not_called()
+
+    for field in db_sessions_mocks:
+        field.commit.assert_not_called()
+        field.rollback.assert_not_called()
+
+    mocked_flash.assert_called_once_with("User did not agree to proceed, action halted.")
+    mocked_logger.exception.assert_not_called()
+
+
+def test_delete_retailer_db_error(
+    mocker: MockerFixture,
+    delete_action: DeleteRetailerAction,
+    test_session_data: SessionTestData,
+    db_sessions_mocks: DBSessionsMocks,
+    delete_action_mocks: DeleteActionMockedDBCalls,
+) -> None:
+    assert delete_action.validate_selected_ids(["1"]) is None
+    assert delete_action.session_data == test_session_data.value
+    delete_action_mocks.get_retailer_by_id.assert_called_once_with(1)
+
+    mocked_flash = mocker.patch("event_horizon.polaris.custom_actions.flash")
+    mocked_logger = mocker.patch.object(delete_action, "logger")
+    delete_action_mocks.delete_carina_retailer_data.side_effect = DataError("test error", [], None)
+    delete_action.form.acceptance.data = True
+
+    delete_action.delete_retailer()
+
+    delete_action_mocks.delete_polaris_retailer_data.assert_called_once()
+    delete_action_mocks.delete_vela_retailer_data.assert_called_once()
+    delete_action_mocks.delete_carina_retailer_data.assert_called_once()
+    delete_action_mocks.delete_hubble_retailer_data.assert_not_called()
+
+    for field in db_sessions_mocks:
+        field.commit.assert_not_called()
+        field.rollback.assert_called_once()
+
+    mocked_flash.assert_called_once_with("Something went wrong, database changes rolled back", category="error")
+    mocked_logger.exception.assert_called_once()
