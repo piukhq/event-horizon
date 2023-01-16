5a1a51ad1f57f5e737f823f41fec334285222523
Haffi Mazhar <hmazhar@bink.com>
2023-01-16 17:39:21 +0000

    BPL-253: Reward File log Admin view

--- event_horizon/carina/db/__init__.py
+++ event_horizon/carina/db/__init__.py
@@ -1,2 +1,11 @@
-from .models import FetchType, Retailer, RetailerFetchType, Reward, RewardCampaign, RewardConfig, RewardUpdate
+from .models import (
+    FetchType,
+    Retailer,
+    RetailerFetchType,
+    Reward,
+    RewardCampaign,
+    RewardConfig,
+    RewardFileLog,
+    RewardUpdate,
+)
 from .session import db_session
